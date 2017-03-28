64bb885991dbe3ea82a668945df3990a94385d9a
Matheus Neves <matneves@publicisgroupe.net>
2017-03-28 19:42:31 -0300

    Update Templates

---
+++ templates/seb/dev/assets/js/app.js
@@ -0,0 +1,37 @@
+import $ from 'jquery';
+//import slick from 'slick-carousel'
+
+export default (() => {
+
+let APP = {
+
+  cache: {
+    ttl: $('.header__ttl')
+  },
+
+  bind: () => {
+    APP.funcs.helloWorld();
+  },
+
+  funcs: {
+
+    helloWorld: () => {
+      APP.cache.ttl.text('Hello World!');
+    }
+
+  },
+
+  plugins: () => {
+
+  },
+
+  init: () => {
+    APP.plugins();
+    APP.funcs;
+    APP.bind();
+  }
+};
+
+APP.init();
+
+})();
